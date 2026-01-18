805db26df60ef669dccc7e91bacdce5e02fbd3aa
Vũ Vương <44000492+ung0v@users.noreply.github.com>
2026-01-18 17:04:10 +0700

    Update Cognito configuration in config.js
    Updated AWS Cognito configuration with new user pool details.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_jlrcYPQrt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4a56gpd6sbt9u8s33odknjqofq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
